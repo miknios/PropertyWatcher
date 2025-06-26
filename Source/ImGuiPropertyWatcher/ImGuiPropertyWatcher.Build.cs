31da4e55e014e23b1579db3cd27e95852f1e46c3
Mikolaj Wojcik <miknios@gmail.com>
2025-06-27 01:05:19 +0200

    Created UE plugin from existing source

---
+++ Source/ImGuiPropertyWatcher/ImGuiPropertyWatcher.Build.cs
@@ -0,0 +1,31 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ImGuiPropertyWatcher : ModuleRules
+{
+	public ImGuiPropertyWatcher(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"ImGui",
+
+				"Core",
+			}
+		);
+
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+			}
+		);
+	}
+}
---
+++ Source/ImGuiPropertyWatcher/Private/ImGuiPropertyWatcher.cpp
@@ -0,0 +1,17 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ImGuiPropertyWatcher.h"
+
+#define LOCTEXT_NAMESPACE "FImGuiPropertyWatcherModule"
+
+void FImGuiPropertyWatcherModule::StartupModule()
+{
+}
+
+void FImGuiPropertyWatcherModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FImGuiPropertyWatcherModule, ImGuiPropertyWatcher)
